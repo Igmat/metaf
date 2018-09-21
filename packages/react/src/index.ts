23bb4483b2d23e7a71f93e7035b559bc7e98b171
Ihor Chulinda <ichulinda@gmail.com>
2018-09-21 19:36:24 +0200

    fix(react): with requirements hoc

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,4 +2,4 @@ export { MockRoot } from './MockRoot';
 export { ApplicationRoot } from './ApplicationRoot';
 export { ResolvableComponent } from './ResolvableComponent';
 export { override as dependency } from 'metaf-resolvable';
-export { overrideRequirement as requirement } from './Resolver';
+export { overrideRequirement as requirement, resolveRequirements as withRequirements } from './Resolver';
