4c1b419ebc772f672e58e9a92afe207830e53325
Ihor Chulinda <ichulinda@gmail.com>
2018-09-21 19:36:51 +0200

    fix(react): improve syntax for resolvable component

--- packages/react/src/ResolvableComponent.ts
+++ packages/react/src/ResolvableComponent.ts
@@ -5,17 +5,26 @@ import { IRequirements } from './Resolver';
 export type ResolvableComponent<I extends IInjections> =
     new <P = {}, S = {}, SS = any>(props: Readonly<P>) => AbstractResolvable<I> & Component<P, S, SS>;
 
-/**
- * // TODO: comment ResolvableComponent
- * @description Resolvable component
- * @template I
- * @template R
- * @param [injections]
- * @param requirements
- * @returns component
- */
-export function ResolvableComponent<I extends IInjections = {}, R extends IRequirements = []>(
-    injections: I = {} as I,
-    ...requirements: R): ResolvableComponent<I> {
-    return Resolvable(injections, Component, ...requirements) as ResolvableComponent<I>;
+export interface IResolvableComponent {
+    /**
+     * // TODO: comment ResolvableComponent
+     * @description Resolvable component
+     * @template I
+     * @template R
+     * @param [injections] dependencies that will be used by class directly
+     * @param requirements dependencies that should exist in code that use this class
+     * @returns component class to extend
+     */
+    <I extends IInjections = {}, R extends IRequirements = []>(injections?: I, ...requirements: R): ResolvableComponent<I>;
+    /**
+     * // TODO: comment ResolvableComponent
+     * @description Resolvable component
+     * @template I
+     * @template R
+     * @param requirements dependencies that should exist in code that use this class
+     * @returns component class to extend
+     */
+    <I extends IInjections = {}, R extends IRequirements = []>(...requirements: R): ResolvableComponent<I>;
 }
+// tslint:disable-next-line:variable-name
+export const ResolvableComponent = Resolvable(Component) as IResolvableComponent;
