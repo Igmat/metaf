fd331346fa4fd84ee09e8f1ac9988e5a1769d6c6
Artem Platonov <artem.platonov@zocdoc.com>
2018-09-23 17:18:12 +0200

    fix(react): remove optional children prop

--- packages/react/src/defineRequirement.tsx
+++ packages/react/src/defineRequirement.tsx
@@ -1,7 +1,5 @@
 import React, { ReactNode } from 'react';
 
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
-
-export const defineRequirement = <T extends {children?: ReactNode}>(Requirement: React.ComponentType<T>, props?: Omit<T, 'children'>) =>
+export const defineRequirement = <T extends {}>(Requirement: React.ComponentType<T>, props?: Pick<T, Exclude<keyof T, 'children'>>) =>
     (app: ReactNode) =>
         <Requirement { ...props }>{ app }</Requirement>;
