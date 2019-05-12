2749125d5a08b6a6af4350cebac37ad219f75d9d
Ihor Chulinda <ichulinda@gmail.com>
2019-05-12 21:10:48 +0200

    test(react): update todo-react test case

--- tests/todo-react/src/App.tsx
+++ tests/todo-react/src/App.tsx
@@ -1,85 +1,33 @@
-// tslint:disable
-import { ResolvableComponent, MockRoot, dependency, requirement } from 'metaf-react';
-import React, { Component } from 'react';
+import { ResolvableComponent } from 'metaf-react';
+import React from 'react';
 import './App.css';
 
-// import logo from './logo.svg';
 class MyService {
-    public somedata = 'Hello from Metaf';
-    public i = 0;
-    public getI() {
-        console.log(this.somedata)
-        return this.i;
-    }
+    i = 1;
+    arr = [{
+        a: 1,
+    }];
+    obj = {
+        j: 2,
+    };
 }
-const Item = ({ item }: any) => (item > 10 && item < 15)
-    ? <div>Component<MyComponent /></div>
-    : <div>{item}</div>
 
-const getItem = () => Item;
-function simpleHoc(app: any) {
-    return <div style={{ fontWeight: 'bold' }}>{app}</div>
-}
-class MyComponent extends ResolvableComponent({ MyService, Item: getItem }, simpleHoc) {
-    public state = {
-        i: this.dependencies.MyService.getI(),
-    }
-    timer: any;
-    constructor(props: any) {
-        super(props);
-    }
-    public render() {
-        return (
-            <div>
-                <Child item={this.state.i} />
-                <this.dependencies.Item item={30} />
-            </div>
-        );
-    }
-
-    componentDidMount() {
-        //this.timer = setTimeout(() => this.setState({ i: 11 }), 10000)
-    }
-    componentWillUnmount() {
-        clearInterval(this.timer);
-    }
-}
-class Child extends Component<{ item: number }> {
+export default class App extends ResolvableComponent({ MyService }) {
     render() {
-        return <Item item={this.props.item} />
-    }
-}
+        const { MyService } = this.dependencies;
 
-class App extends React.Component {
-    public render() {
         return (
             <div>
-                <MockRoot>
-                    <MockRoot
-                        dependencies={[
-                            dependency(MyService, { somedata: 'hello from mock', i: 1, getI: () => 3 })
-                        ]}
-                        requirements={[
-                            requirement(simpleHoc, (app: any) => <div style={{ color: 'green' }}>{app}</div>)
-                        ]}
-                    >
-                        <MyComponent />
-                    </MockRoot>
-                    <MyComponent />
-                    <MockRoot
-                        dependencies={[
-                            dependency(MyService, { somedata: 'hello from mock', i: 1, getI: () => 3 })
-                        ]}
-                        requirements={[
-                            requirement(simpleHoc, (app: any) => <div style={{ color: 'red' }}>{app}</div>)
-                        ]}
-                    >
-                        <MyComponent />
-                    </MockRoot>
-                </MockRoot>
+                <h1>{MyService.i} ({MyService.obj.j})</h1>
+                {MyService.arr.map(({a}, index) => <h2 key={index}>{a}</h2>)}
+                <button onClick={() => MyService.i++}>increment</button>
+                <button onClick={() => MyService.arr.push({ a: MyService.i})}>add</button>
+                <button onClick={() => MyService.obj.j++}>increment2</button>
+                <button onClick={() => MyService.obj = { j: 1 }}>reset obj</button>
+                <button onClick={() => MyService.arr = []}>reset arr</button>
+                <button onClick={() => MyService.arr[MyService.obj.j].a = MyService.i}>reset arr ent number</button>
+                <button onClick={() => MyService.arr[MyService.obj.j] = { a: MyService.i}}>reset arr ent whole</button>
             </div>
         );
     }
 }
-
-export default App;
--- tests/todo-react/src/index.tsx
+++ tests/todo-react/src/index.tsx
@@ -1,13 +1,11 @@
 
-// import { ApplicationRoot } from 'metaf-react';
+import { ApplicationRoot } from 'metaf-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-    <App />,
-    document.getElementById('root') as HTMLElement
+    <ApplicationRoot><App /></ApplicationRoot>,
+    document.getElementById('root') as HTMLElement,
 );
-registerServiceWorker();
