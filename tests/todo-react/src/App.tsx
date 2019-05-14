35f779676d3c88f12ed36ef6717075fad9455d38
Ihor Chulinda <ichulinda@gmail.com>
2019-05-14 14:37:40 +0200

    test(react): update test project for new API

--- tests/todo-react/src/App.tsx
+++ tests/todo-react/src/App.tsx
@@ -1,4 +1,4 @@
-import { ResolvableComponent } from 'metaf-react';
+import { Component } from 'metaf-react';
 import React from 'react';
 import './App.css';
 
@@ -10,15 +10,19 @@ class MyService {
     obj = {
         j: 2,
     };
+    cs = new Promise<number>(resolve => resolve(1));
+    async f() {
+        return (await this.cs) * this.i;
+    }
 }
 
-export default class App extends ResolvableComponent({ MyService }) {
+export default class App extends Component({ MyService }) {
     render() {
         const { MyService } = this.dependencies;
 
         return (
             <div>
-                <h1>{MyService.i} ({MyService.obj.j})</h1>
+                <h1>{MyService.i} ({MyService.obj.j}) {MyService.f().value}</h1>
                 {MyService.arr.map(({a}, index) => <h2 key={index}>{a}</h2>)}
                 <button onClick={() => MyService.i++}>increment</button>
                 <button onClick={() => MyService.arr.push({ a: MyService.i})}>add</button>
