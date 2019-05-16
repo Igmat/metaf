c68c3329d820278afe86f32b630f75fcd4be139b
Ihor Chulinda <ichulinda@gmail.com>
2019-05-16 12:59:28 +0200

    test(react): add few more cases to test project

--- tests/todo-react/src/App.tsx
+++ tests/todo-react/src/App.tsx
@@ -1,14 +1,14 @@
-import { Component } from 'metaf-react';
+import { Component, State } from 'metaf-react';
 import React from 'react';
 import './App.css';
 
-class MyService {
+class MyService extends State() {
     i = 1;
     arr = [{
         a: 1,
     }];
     obj = {
-        j: 2,
+        a: 2,
     };
     cs = new Promise<number>(resolve => resolve(1));
     async f() {
@@ -22,15 +22,16 @@ export default class App extends Component({ MyService }) {
 
         return (
             <div>
-                <h1>{MyService.i} ({MyService.obj.j}) {MyService.f().value}</h1>
+                <h1>{MyService.i} ({MyService.obj.a}) {MyService.f().value}</h1>
                 {MyService.arr.map(({a}, index) => <h2 key={index}>{a}</h2>)}
                 <button onClick={() => MyService.i++}>increment</button>
-                <button onClick={() => MyService.arr.push({ a: MyService.i})}>add</button>
-                <button onClick={() => MyService.obj.j++}>increment2</button>
-                <button onClick={() => MyService.obj = { j: 1 }}>reset obj</button>
+                <button onClick={() => MyService.arr.push({ a: MyService.i })}>add</button>
+                <button onClick={() => MyService.arr.push(MyService.obj)}>add obj</button>
+                <button onClick={() => MyService.obj.a++}>increment2</button>
+                <button onClick={() => MyService.obj = { a: 1 }}>reset obj</button>
                 <button onClick={() => MyService.arr = []}>reset arr</button>
-                <button onClick={() => MyService.arr[MyService.obj.j].a = MyService.i}>reset arr ent number</button>
-                <button onClick={() => MyService.arr[MyService.obj.j] = { a: MyService.i}}>reset arr ent whole</button>
+                <button onClick={() => MyService.arr[MyService.obj.a].a = MyService.i}>reset arr ent number</button>
+                <button onClick={() => MyService.arr[MyService.obj.a] = { a: MyService.i}}>reset arr ent whole</button>
             </div>
         );
     }
