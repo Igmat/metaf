f168fa1bdbd37f2bf790bc27182bf8147e08abae
Ihor Chulinda <ichulinda@gmail.com>
2019-06-09 19:01:56 +0200

    fix(resolvable): change styles for linter

--- packages/resolvable/src/index.ts
+++ packages/resolvable/src/index.ts
@@ -1,5 +1,5 @@
+export * from './AbstractResolvable';
 export { setResolverFn } from './Application';
 export { Resolvable } from './Resolvable';
 export * from './Resolver';
-export * from './AbstractResolvable';
 export * from './utils';
